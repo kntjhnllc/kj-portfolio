c9f19ffecff0e8843e2ee14ce8281c14d2f86a18
kenjanjan <kjliloc@hcdc.edu.ph>
2024-04-09 16:36:33 +0800

    footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-[#2d2f33] py-10 ">
+      <div className="wrapper ">
+        <div className="flex justify-between items-center text-white px-52 ml-1">
+          <p>Copyright © 2024. All rights are reserved</p>
+          <div className="flex items-center gap-2">
+            <Link
+              href={"https://www.linkedin.com/in/kent-john-liloc-28530923a/"}
+            >
+              <img
+                src="/icons/linkedin-white.png"
+                alt="linkedin"
+                className="cursor-pointer"
+              />
+            </Link>
+            <Link href={"https://github.com/kntjhnllc"}>
+              <img
+                src="/icons/github-white.png"
+                alt="github"
+                className="h-[40px] cursor-pointer"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
-import NavItems from "../NavItems";
-import MobileNav from "../MobileNav";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
